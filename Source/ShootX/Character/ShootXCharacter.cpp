614f8e58ab0b3f8fabe9e249802901895f38cf44
Hitarth Mistry <hitarthmistry42@gmail.com>
2022-08-19 17:38:39 -0500

    Overhead Widget and Network Role
    Created an overhead widget for the characters that displays the remote role of the character, ie. Authority, Autonomous Proxy, and Simulated Proxy

--- Source/ShootX/Character/ShootXCharacter.cpp
+++ Source/ShootX/Character/ShootXCharacter.cpp
@@ -5,6 +5,7 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Components/WidgetComponent.h"
 
 AShootXCharacter::AShootXCharacter()
 {
@@ -21,6 +22,9 @@ AShootXCharacter::AShootXCharacter()
 
 	bUseControllerRotationYaw = false;
 	GetCharacterMovement()->bOrientRotationToMovement = true;
+
+	OverheadWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("OverheadWidget"));
+	OverheadWidget->SetupAttachment(RootComponent);
 }
 
 void AShootXCharacter::BeginPlay()
