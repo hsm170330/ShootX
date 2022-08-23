0e634822a6209a01db9a5c91f20c7ec465567024
Hitarth Mistry <hitarthmistry42@gmail.com>
2022-08-23 14:15:51 -0500

    Aim Offset Poses
    Created Aim Offset Poses that are just single frame poses and put them into Aim Offset Blendspaces, one for Hipfire and one for ADS.
    
    Hooked up aiming into the animation bp
    
    Fixed the issue of the character having the moving animation still while aiming when going from aim walking or aim crouch walking to aim idle in both cases.

--- Source/ShootX/Character/ShootXAnimInstance.h
+++ Source/ShootX/Character/ShootXAnimInstance.h
@@ -48,4 +48,10 @@ private:
 	FRotator CharacterRotationLastFrame;
 	FRotator CharacterRotation;
 	FRotator DeltaRotation;
+
+	UPROPERTY(BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true"))
+	float AO_Yaw;
+
+	UPROPERTY(BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true"))
+	float AO_Pitch;
 };
